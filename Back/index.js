666f342bbbb5d566aa112bb6ffcd35d338174a21
Anciiicaa <anchi_stojadinovic97@yahoo.com>
2023-12-01 12:36:15 +0100

    nova
    da

--- Back/index.js
+++ Back/index.js
@@ -1,1 +1,38 @@
-console.log("Anica")
+const sql = require('mssql');
+
+// Postavite informacije o konekciji
+const config = {
+    server: '127.0.0.1\\MSSQLSERVER01',
+    database: 'HR',
+    options: {
+      encrypt: true,
+      trustServerCertificate: true,
+      authentication: {
+        type: 'Windows',
+      },
+    },
+  };
+  
+  
+
+// Napravite konekciju
+sql.connect(config, (err) => {
+  if (err) {
+    console.error('Greška pri povezivanju:', err);
+    return;
+  }
+
+  // Ovde možete izvršiti SQL upite
+  // Na primer:
+  const request = new sql.Request();
+  request.query('SELECT * FROM Employee', (err, result) => {
+    if (err) {
+      console.error('Greška pri izvršavanju upita:', err);
+      sql.close();
+      return;
+    }
+
+    console.log('Rezultat upita:', result.recordset);
+    sql.close();
+  });
+});
